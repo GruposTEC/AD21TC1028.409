3c78804512849ba2599a039a7a7fb6cfa6b73d8e
Antonio Mejia <avmejia@tec.mx>
2021-08-23 10:33:47 -0500

    Python tutor

---
+++ 3.-Funciones/tutor.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 23 10:33:21 2021
+
+@author: avmejia
+"""
+
+var_global = 5
+
+def mayor2(x , y):
+    global var_global
+    
+    var_global = 3
+    temp = 0;
+    if x > y :
+        temp = x
+    else:
+        temp = y
+    
+    x = x + 1
+    y = y + 1
+    
+    return temp  
+    
+primera = 10;
+segunda = 5
+
+mayor = mayor2(primera,segunda)
+
+print(mayor)
